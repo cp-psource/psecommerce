15c67a068407e058cd9ecebc5c79669b109f5bf5
DerN3rd <dern3rd@outlook.at>
2023-01-25 15:34:47 +0100

    Varations-Overhaul

--- includes/admin/ui/js/admin-product-variation.js
+++ includes/admin/ui/js/admin-product-variation.js
@@ -1,4 +1,4 @@
-jQuery( document ).ready( function( $ ) {
-    jQuery( '#wpbody-content .wrap h2' ).html( $( '#variation_title' ).val() );
-    jQuery( '#wpbody-content .wrap h2' ).show();
-} );
+$(document).ready(function(){
+    $('#wpbody-content .wrap h2').html($('#variation_title').val());
+    $('#wpbody-content .wrap h2').show();
+});
