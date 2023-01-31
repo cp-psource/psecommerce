22605e4da433148578eb9c4ebce1856e122acfa6
DerN3rd <dern3rd@outlook.at>
2023-01-31 05:00:53 +0100

    Redundand gelöscht

--- includes/includes/addons/mp-pdf-invoice/templates/default/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Name: Default
- * Description: Default template for PSeCommerce invoice
- */
-//only for information
--- includes/includes/admin/ui/js/admin-product-variation.js
+++
@@ -1,4 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-    jQuery( '#wpbody-content .wrap h2' ).html( $( '#variation_title' ).val() );
-    jQuery( '#wpbody-content .wrap h2' ).show();
-} );
